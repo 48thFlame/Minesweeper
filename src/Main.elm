8d9e15d2ed2908ff9ff79ea2d5b4630aecedef11
Avishai Taragin <79379047+48thFlame@users.noreply.github.com>
2023-09-22 12:42:12 +0300

    Displays grid

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,8 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (..)
+import Html exposing (Html, div)
+import Html.Attributes exposing (class, style)
 
 
 main : Program () Model Msg
@@ -15,14 +16,24 @@ main =
 
 
 type alias Model =
-    { property : Int
-    , property2 : String
+    { grid : List (List Cell) }
+
+
+type alias Cell =
+    { color : String
     }
 
 
 init : flags -> ( Model, Cmd Msg )
 init _ =
-    ( Model 0 "modelInitialValue2", Cmd.none )
+    let
+        row =
+            List.repeat 7 { color = "cadetblue" }
+
+        grid =
+            List.repeat 6 row
+    in
+    ( Model grid, Cmd.none )
 
 
 type Msg
@@ -45,7 +56,36 @@ subscriptions _ =
     Sub.none
 
 
+gridToHtml : Model -> Html Msg
+gridToHtml model =
+    let
+        cellToHtml rowI colI cell =
+            let
+                rowNum =
+                    (rowI + 1) |> String.fromInt
+
+                colNum =
+                    (colI + 1) |> String.fromInt
+            in
+            div
+                [ class "cell"
+                , style "grid-row-start" rowNum
+                , style "grid-column-start" colNum
+
+                -- , class ("cell-row-" ++ rowNum)
+                -- , class ("cell-col-" ++ rowNum)
+                ]
+                []
+
+        rowToHtml rowI row =
+            List.indexedMap (cellToHtml rowI) row
+    in
+    div [ class "grid_area" ] (List.indexedMap rowToHtml model.grid |> List.concat)
+
+
 view : Model -> Html Msg
 view model =
-    div []
-        [ text "New Element" ]
+    div [ class "main" ]
+        [ gridToHtml
+            model
+        ]
