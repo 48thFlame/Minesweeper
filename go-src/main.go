ff6032bdd912b15ff8362d491df9c0bc1eb043dc
Avishai Taragin <79379047+48thFlame@users.noreply.github.com>
2023-09-29 17:41:09 +0300

    Random grid is generated(WASM)  & displayed(Elm)

--- go-src/main.go
+++ go-src/main.go
@@ -2,27 +2,52 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"syscall/js"
 )
 
 func main() {
 	done := make(chan struct{})
-	js.Global().Set("add", js.FuncOf(add))
+
+	js.Global().Set("newGrid", js.FuncOf(newGrid))
+
 	<-done
-}
-
-func add(this js.Value, args []js.Value) interface{} {
-	defer doNotPanicPlease()
-
-	a := args[0].Float()
-	b := args[1].Float()
-
-	return a + b
 }
 
 func doNotPanicPlease() {
 	r := recover()
+
 	if r != nil {
 		fmt.Println("Recovered from", r)
 	}
+}
+
+// Cells:
+// 0..8 = An open cell and its number represents how many mines are around.
+// 9 = Mine Cell
+// 10 = Closed cell.
+// 11 = Flag cell.
+type Cell = int
+
+// `newGrid(rows_num, cols_num)` expects 2 args: rows_num and cols_num, both need to be ints.
+// Can't return a nested array threw WASM (couldn't figure this one out)
+// so returns a 1-D array where the rows are separated by -1
+func newGrid(this js.Value, args []js.Value) any {
+	defer doNotPanicPlease()
+
+	rows_num := args[0].Int()
+	cols_num := args[1].Int()
+
+	grid := make([]any, 0)
+
+	for rowI := 0; rowI < rows_num; rowI++ {
+		for colI := 0; colI < cols_num; colI++ {
+			cell := rand.Intn(12)
+
+			grid = append(grid, cell)
+
+		}
+	}
+
+	return grid
 }
--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,8 @@
-module Main exposing (main)
+port module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div)
-import Html.Attributes exposing (class, style)
+import Html exposing (Html, div, img, text)
+import Html.Attributes exposing (class, src)
 
 
 main : Program () Model Msg
@@ -15,72 +15,122 @@ main =
         }
 
 
+port newGrid : ( Int, Int ) -> Cmd msg
+
+
+port gridReceiver : (List Int -> msg) -> Sub msg
+
+
+
+-- i = cols_num * (row ) + col
+
+
 type alias Model =
-    { grid : List (List Cell) }
+    { grid : List Cell }
 
 
-type alias Cell =
-    { color : String
-    }
+type Cell
+    = Closed
+    | Number Int
+    | Flag
+    | Mine
 
 
 init : flags -> ( Model, Cmd Msg )
 init _ =
-    let
-        row =
-            List.repeat 7 { color = "cadetblue" }
-
-        grid =
-            List.repeat 6 row
-    in
-    ( Model grid, Cmd.none )
+    -- let
+    -- row =
+    --     List.repeat 7 Flag
+    -- -- List.repeat 7 (Number 6)
+    -- grid =
+    --     List.repeat 6 row
+    -- grid =
+    --     [ [ Number 6, Closed, Closed, Closed, Flag, Number 6, Number 6 ]
+    --     , [ Number 6, Number 6, Number 5, Number 6, Number 6, Closed, Number 6 ]
+    --     , [ Flag, Number 6, Number 6, Number 6, Number 6, Number 6, Number 6 ]
+    --     , [ Number 6, Number 6, Closed, Number 6, Number 6, Flag, Number 6 ]
+    --     , [ Number 6, Number 6, Closed, Flag, Number 6, Number 6, Number 6 ]
+    --     , [ Number 6, Number 6, Number 6, Flag, Flag, Number 6, Closed ]
+    --     ]
+    -- in
+    ( Model [], newGrid ( 6, 7 ) )
 
 
 type Msg
-    = Msg1
-    | Msg2
+    = NewGrid (List Int)
+
+
+intToCell : Int -> Cell
+intToCell i =
+    case i of
+        9 ->
+            Mine
+
+        10 ->
+            Closed
+
+        11 ->
+            Flag
+
+        num ->
+            Number num
+
+
+decodeIncomingGrid : List Int -> List Cell
+decodeIncomingGrid incGrid =
+    List.map intToCell incGrid
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Msg1 ->
-            ( model, Cmd.none )
-
-        Msg2 ->
-            ( model, Cmd.none )
+        NewGrid grid ->
+            let
+                _ =
+                    Debug.log "grid" grid
+            in
+            ( { model | grid = decodeIncomingGrid grid }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    Sub.none
+    gridReceiver NewGrid
+
+
+cellToHtml : Cell -> Html Msg
+cellToHtml cell =
+    case cell of
+        Closed ->
+            div [ class "cell", class "closed-cell" ] []
+
+        Number num ->
+            div [ class "cell", class "number-cell" ]
+                [ String.fromInt num |> text ]
+
+        Flag ->
+            div [ class "cell", class "flag-cell" ]
+                [ img [ src "assets/flag.svg", class "cell-img" ] [] ]
+
+        Mine ->
+            div [ class "cell", class "mine-cell" ]
+                [ img [ src "assets/mine.svg", class "cell-img" ] [] ]
 
 
 gridToHtml : Model -> Html Msg
 gridToHtml model =
-    let
-        cellToHtml rowI colI cell =
-            let
-                rowNum =
-                    (rowI + 1) |> String.fromInt
-
-                colNum =
-                    (colI + 1) |> String.fromInt
-            in
-            div
-                [ class "cell"
-                , style "grid-row-start" rowNum
-                , style "grid-column-start" colNum
-
-                -- , class ("cell-row-" ++ rowNum)
-                -- , class ("cell-col-" ++ rowNum)
-                ]
-                []
-
-        rowToHtml rowI row =
-            List.indexedMap (cellToHtml rowI) row
-    in
-    div [ class "grid_area" ] (List.indexedMap rowToHtml model.grid |> List.concat)
+    -- createGridSlot rowI colI cell =
+    --     let
+    --         rowNum =
+    --             (rowI + 1) |> String.fromInt
+    --         colNum =
+    --             (colI + 1) |> String.fromInt
+    --     in
+    --     div
+    --         [ style "grid-row-start" rowNum
+    --         , style "grid-column-start" colNum
+    --         ]
+    --         [ cellToHtml cell ]
+    div [ class "grid_area" ] (List.map cellToHtml model.grid)
 
 
 view : Model -> Html Msg
