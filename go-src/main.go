ffd00fb1ed3ebaa69bc97a78e49983dae7bbc086
Avishai Taragin <79379047+48thFlame@users.noreply.github.com>
2023-10-10 15:15:49 +0300

    New `newMines` WASM func, grid display is smart

--- go-src/main.go
+++ go-src/main.go
@@ -10,6 +10,7 @@ func main() {
 	done := make(chan struct{})
 
 	js.Global().Set("newGrid", js.FuncOf(newGrid))
+	js.Global().Set("newMines", js.FuncOf(newMines))
 
 	<-done
 }
@@ -31,14 +32,14 @@ type Cell = int
 
 // `newGrid(rows_num, cols_num)` expects 2 args: rows_num and cols_num, both need to be ints.
 // Can't return a nested array threw WASM (couldn't figure this one out)
-// so returns a 1-D array where the rows are separated by -1
+// so returns a 1-D array
 func newGrid(this js.Value, args []js.Value) any {
 	defer doNotPanicPlease()
 
 	rows_num := args[0].Int()
 	cols_num := args[1].Int()
 
-	grid := make([]any, 0)
+	grid := make([]any, 0, rows_num*cols_num)
 
 	for rowI := 0; rowI < rows_num; rowI++ {
 		for colI := 0; colI < cols_num; colI++ {
@@ -50,4 +51,47 @@ func newGrid(this js.Value, args []js.Value) any {
 	}
 
 	return grid
+}
+
+// newRange(low, high) returns a slice of range from [low, high).
+// Panics if high < low!
+func newRange(low, high int) []any {
+	r := make([]any, 0, (high - low))
+
+	for num := low; num < high; num++ {
+		r = append(r, num)
+	}
+
+	return r
+}
+
+// remove(s, i) removes the item at index `i` from slice `s`.
+// ** NOTE: messes up the slice order!
+func remove(s []any, i int) []any {
+	s[i] = s[len(s)-1]
+	return s[:len(s)-1]
+}
+
+// newMines(rows_num, cols_num) expects 2 args: rows_num and cols_num, both need to be ints.
+// Returns an array of 1-D array indexes that should be mines.
+func newMines(this js.Value, args []js.Value) any {
+	defer doNotPanicPlease()
+
+	rows_num := args[0].Int()
+	cols_num := args[1].Int()
+
+	totalMines := int(rows_num * cols_num / 10)
+
+	locationPool := newRange(0, rows_num*cols_num)
+	mines := make([]any, 0, totalMines)
+
+	for a := 0; a < totalMines; a++ {
+		locI := rand.Intn(len(locationPool))
+		mine := locationPool[locI]
+
+		locationPool = remove(locationPool, locI)
+		mines = append(mines, mine)
+	}
+
+	return mines
 }
--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,7 @@ port module Main exposing (main)
 
 import Browser
 import Html exposing (Html, div, img, text)
-import Html.Attributes exposing (class, src)
+import Html.Attributes exposing (class, src, style)
 
 
 main : Program () Model Msg
@@ -15,20 +15,43 @@ main =
         }
 
 
-port newGrid : ( Int, Int ) -> Cmd msg
+{-| `gridGetter` is an incoming port to get updated grid to display, for example after sends `updateGrid` because new cell was opened.
+-}
+port gridGetter : (List Int -> msg) -> Sub msg
 
 
-port gridReceiver : (List Int -> msg) -> Sub msg
+{-| `minesGetter` is an incoming port to get new mines after starting a new game.
+-}
+port minesGetter : (List Int -> msg) -> Sub msg
 
 
+{-| `newGame` is an outgoing port to get a random list of mine locations.
+-}
+port newGame : { rowsNum : Int, colsNum : Int } -> Cmd msg
 
--- i = cols_num * (row ) + col
+
+{-| `updateGrid` is an outgoing port to update the grid because new cell was opened.
+-}
+port updateGrid :
+    { rowsNum : Int
+    , colsNum : Int
+    , mines : List Int
+    , opened : List Int
+    }
+    -> Cmd msg
 
 
 type alias Model =
-    { grid : List Cell }
+    { grid : List Cell
+    , rowsNum : Int
+    , colsNum : Int
+    , mines : List Int
+    , opened : List Int
+    }
 
 
+{-| `Cell` is a cell of the grid top to display
+-}
 type Cell
     = Closed
     | Number Int
@@ -38,46 +61,47 @@ type Cell
 
 init : flags -> ( Model, Cmd Msg )
 init _ =
-    -- let
-    -- row =
-    --     List.repeat 7 Flag
-    -- -- List.repeat 7 (Number 6)
-    -- grid =
-    --     List.repeat 6 row
-    -- grid =
-    --     [ [ Number 6, Closed, Closed, Closed, Flag, Number 6, Number 6 ]
-    --     , [ Number 6, Number 6, Number 5, Number 6, Number 6, Closed, Number 6 ]
-    --     , [ Flag, Number 6, Number 6, Number 6, Number 6, Number 6, Number 6 ]
-    --     , [ Number 6, Number 6, Closed, Number 6, Number 6, Flag, Number 6 ]
-    --     , [ Number 6, Number 6, Closed, Flag, Number 6, Number 6, Number 6 ]
-    --     , [ Number 6, Number 6, Number 6, Flag, Flag, Number 6, Closed ]
-    --     ]
-    -- in
-    ( Model [], newGrid ( 6, 7 ) )
+    let
+        rowsNum =
+            6
+
+        colsNum =
+            7
+    in
+    ( { grid = []
+      , rowsNum = rowsNum
+      , colsNum = colsNum
+      , mines = []
+      , opened = []
+      }
+    , newGame { rowsNum = rowsNum, colsNum = colsNum }
+    )
 
 
 type Msg
     = NewGrid (List Int)
-
-
-intToCell : Int -> Cell
-intToCell i =
-    case i of
-        9 ->
-            Mine
-
-        10 ->
-            Closed
-
-        11 ->
-            Flag
-
-        num ->
-            Number num
+    | NewMines (List Int)
+    | OpenCell Int
 
 
 decodeIncomingGrid : List Int -> List Cell
 decodeIncomingGrid incGrid =
+    let
+        intToCell : Int -> Cell
+        intToCell i =
+            case i of
+                9 ->
+                    Mine
+
+                10 ->
+                    Closed
+
+                11 ->
+                    Flag
+
+                num ->
+                    Number num
+    in
     List.map intToCell incGrid
 
 
@@ -85,52 +109,94 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NewGrid grid ->
-            let
-                _ =
-                    Debug.log "grid" grid
-            in
             ( { model | grid = decodeIncomingGrid grid }, Cmd.none )
+
+        NewMines mines ->
+            ( { model | mines = mines |> Debug.log "mines" }, Cmd.none )
+
+        OpenCell _ ->
+            ( model, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    gridReceiver NewGrid
+    Sub.batch
+        [ gridGetter NewGrid
+        , minesGetter NewMines
+        ]
 
 
 cellToHtml : Cell -> Html Msg
 cellToHtml cell =
     case cell of
         Closed ->
-            div [ class "cell", class "closed-cell" ] []
+            div [ class "closed-cell" ] []
 
         Number num ->
-            div [ class "cell", class "number-cell" ]
+            div [ class "number-cell" ]
                 [ String.fromInt num |> text ]
 
         Flag ->
-            div [ class "cell", class "flag-cell" ]
+            div [ class "flag-cell" ]
                 [ img [ src "assets/flag.svg", class "cell-img" ] [] ]
 
         Mine ->
-            div [ class "cell", class "mine-cell" ]
+            div [ class "mine-cell" ]
                 [ img [ src "assets/mine.svg", class "cell-img" ] [] ]
+
+
+
+{-
+   i = cols_num * (row ) + col
+
+   -col = cols_num * row - i
+   col = - (cols_num * row - i)
+   col = -cols_num * row + i
+
+   col = mod(i, cols_num)
+
+   cols_num * row = i - col
+   row = (i - col) / cols_num
+
+   row = floor(i / cols_num)
+-}
 
 
 gridToHtml : Model -> Html Msg
 gridToHtml model =
-    -- createGridSlot rowI colI cell =
-    --     let
-    --         rowNum =
-    --             (rowI + 1) |> String.fromInt
-    --         colNum =
-    --             (colI + 1) |> String.fromInt
-    --     in
-    --     div
-    --         [ style "grid-row-start" rowNum
-    --         , style "grid-column-start" colNum
-    --         ]
-    --         [ cellToHtml cell ]
-    div [ class "grid_area" ] (List.map cellToHtml model.grid)
+    let
+        colsNum =
+            model.colsNum
+
+        getRowNumStr : Int -> String
+        getRowNumStr i =
+            (i // colsNum) + 1 |> String.fromInt
+
+        getColNumStr : Int -> String
+        getColNumStr i =
+            modBy colsNum i + 1 |> String.fromInt
+
+        gridCell i cell =
+            div
+                [ class "cell"
+                , style "grid-row-start" (getRowNumStr i)
+                , style "grid-column-start" (getColNumStr i)
+                ]
+                [ cellToHtml cell ]
+
+        colsNumStr =
+            model.colsNum |> String.fromInt
+
+        rowsNumStr =
+            model.rowsNum |> String.fromInt
+    in
+    div
+        [ class "grid_area"
+        , style "aspect-ratio" (colsNumStr ++ "/" ++ rowsNumStr)
+        , style "grid-template-rows" ("repeat(" ++ rowsNumStr ++ ", 1fr)")
+        , style "grid-template-columns" ("repeat(" ++ colsNumStr ++ ", 1fr)")
+        ]
+        (List.indexedMap gridCell model.grid)
 
 
 view : Model -> Html Msg
