b18a9484a0a6a4048d97665e1850d3a4cbc4f831
Avishai Taragin <79379047+48thFlame@users.noreply.github.com>
2023-11-03 13:10:30 +0200

    On a 10*10 grid counts the mines around each cell

--- go-src/main.go
+++ go-src/main.go
@@ -32,8 +32,8 @@ type Cell = int
 
 const (
 	mineCell   Cell = 9
-	closedCell      = 10
-	flagCell        = 11
+	closedCell Cell = 10
+	flagCell   Cell = 11
 )
 
 // convertToSlice(arr) takes a js array and returns a []int, may panic if bad array.
@@ -59,7 +59,7 @@ func getGrid(this js.Value, args []js.Value) any {
 	colsNum := args[1].Int()
 	mines := convertToSlice(args[2])
 	flagged := convertToSlice(args[3])
-	opened := convertToSlice(args[4])
+	// opened := convertToSlice(args[4])
 
 	gridArea := rowsNum * colsNum
 
@@ -77,12 +77,83 @@ func getGrid(this js.Value, args []js.Value) any {
 		grid[flagI] = flagCell
 	}
 
-	for _, openI := range opened {
-		// TODO: put the number of mines not 0!
-		grid[openI] = 0
+	for i, slot := range grid {
+		if slot == 10 {
+			numOfMines := countMines(grid, rowsNum, colsNum, i)
+
+			grid[i] = numOfMines
+		}
 	}
 
 	return grid
+}
+
+/*
+   i = cols_num * (row ) + col
+
+   -col = cols_num * row - i
+   col = - (cols_num * row - i)
+   col = -cols_num * row + i
+
+   col = mod(i, cols_num)
+
+   cols_num * row = i - col
+   row = (i - col) / cols_num
+
+   row = floor(i / cols_num)
+*/
+
+func countMines(grid []any, rowsNum, colsNum, i int) (amount int) {
+	col := i % colsNum
+	row := i / colsNum
+
+	indexChanges := make([]int, 0, 8)
+
+	switch row {
+	case 0:
+		indexChanges = append(indexChanges, colsNum)
+		switch col {
+		case 0:
+			indexChanges = append(indexChanges, 1, colsNum+1)
+		case colsNum - 1:
+			indexChanges = append(indexChanges, -1, colsNum-1)
+		default:
+			indexChanges = append(indexChanges, 1, -1, colsNum-1, colsNum+1)
+		}
+	case rowsNum - 1:
+		indexChanges = append(indexChanges, -colsNum)
+		switch col {
+		case 0:
+			indexChanges = append(indexChanges, 1, -colsNum+1)
+		case colsNum - 1:
+			indexChanges = append(indexChanges, -1, -colsNum-1)
+		default:
+			indexChanges = append(indexChanges, 1, -1, -colsNum-1, -colsNum+1)
+		}
+	default:
+		indexChanges = append(indexChanges, colsNum, -colsNum)
+		switch col {
+		case 0:
+			indexChanges = append(indexChanges, 1, -colsNum+1)
+		case colsNum - 1:
+			indexChanges = append(indexChanges, -1, -colsNum-1)
+		default:
+			indexChanges = append(indexChanges, 1, -1, colsNum-1, colsNum+1, -colsNum-1, -colsNum+1)
+		}
+	}
+
+	for _, change := range indexChanges {
+		checkI := i + change
+
+		if checkI >= 0 && checkI < len(grid) {
+			// Its a valid index, check if mine
+			if grid[checkI] == mineCell {
+				amount++
+			}
+		}
+	}
+
+	return
 }
 
 // newRange(low, high) returns a slice of range from [low, high).
@@ -112,7 +183,7 @@ func newMines(this js.Value, args []js.Value) any {
 	rows_num := args[0].Int()
 	cols_num := args[1].Int()
 
-	totalMines := int(rows_num * cols_num / 10)
+	totalMines := int(rows_num * cols_num / 5)
 
 	locationPool := newRange(0, rows_num*cols_num)
 	mines := make([]any, 0, totalMines)
